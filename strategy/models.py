c5aa4b9fa0ea31524d2381ace160f1624303c84f
揭思敏 <jsm0834@175game.com>
2018-05-20 17:21:02 +0800

    #3 Symbol model 增加base_asset、quote_asset 字段
    base_asset 与 quote_asset 用来区分交易对、反交易对

--- strategy/models.py
+++ strategy/models.py
@@ -12,5 +12,7 @@ class Strategy(models.Model):
 
 class Symbol(models.Model):
     name = models.CharField(max_length=30)
+    base_asset = models.CharField(max_length=15)
+    quote_asset = models.CharField(max_length=15)
     min_qty = models.DecimalField(max_digits=14, decimal_places=8)
     step_size = models.DecimalField(max_digits=14, decimal_places=8)
