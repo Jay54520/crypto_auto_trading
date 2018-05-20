51e0c1dc9683ce95425f74661f6951d44dcd29c7
揭思敏 <jsm0834@175game.com>
2018-05-20 14:05:05 +0800

    #2 添加交易策略 model

--- strategy/models.py
+++ strategy/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Strategy(models.Model):
+    symbol = models.CharField(max_length=30)
+    quantity = models.DecimalField(max_digits=14, decimal_places=8)
+    start_dt = models.DateTimeField()
+    end_dt = models.DateTimeField()
