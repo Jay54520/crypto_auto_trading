11cfe5f9fc7168f7c93d6f0ab4381b3f6755f296
揭思敏 <jsm0834@175game.com>
2018-05-20 21:25:11 +0800

    #2 将交易策略中的 symbol 改为外键到 Symbol model

--- strategy/models.py
+++ strategy/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 # Create your models here.
 
-class Strategy(models.Model):
-    symbol = models.CharField(max_length=30)
-    quantity = models.DecimalField(max_digits=14, decimal_places=8)
-    start_dt = models.DateTimeField()
-    end_dt = models.DateTimeField()
-
-
 class Symbol(models.Model):
     name = models.CharField(max_length=30)
     base_asset = models.CharField(max_length=15)
     quote_asset = models.CharField(max_length=15)
     min_qty = models.DecimalField(max_digits=14, decimal_places=8)
     step_size = models.DecimalField(max_digits=14, decimal_places=8)
+
+
+class Strategy(models.Model):
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=14, decimal_places=8)
+    start_dt = models.DateTimeField()
+    end_dt = models.DateTimeField()
