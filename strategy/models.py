8589ccb858b204da8ef5aa6195816b9d7f47ec9e
揭思敏 <jsm0834@175game.com>
2018-05-20 16:57:44 +0800

    #3 增加 Symbol model

--- strategy/models.py
+++ strategy/models.py
@@ -8,3 +8,9 @@ class Strategy(models.Model):
     quantity = models.DecimalField(max_digits=14, decimal_places=8)
     start_dt = models.DateTimeField()
     end_dt = models.DateTimeField()
+
+
+class Symbol(models.Model):
+    name = models.CharField(max_length=30)
+    min_qty = models.DecimalField(max_digits=14, decimal_places=8)
+    step_size = models.DecimalField(max_digits=14, decimal_places=8)
