f59ac6040c6e6b72c7599ebb30d45b79bda5e036
揭思敏 <jsm0834@175game.com>
2018-05-23 22:00:25 +0800

    增加 Django admin
    参考 Django 自带的 UserAdmin 以及展示所有字段：https://stackoverflow.com/a/18774202/5238892

--- strategy/admin.py
+++ strategy/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from strategy.models import Strategy, Order, Symbol
+
+
+@admin.register(Symbol)
+class SymbolAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Symbol._meta.get_fields()]
+    list_filter = ('base_asset', 'quote_asset')
+    search_fields = ('name', 'base_asset', 'quote_asset')
+    ordering = ('name', 'base_asset', 'quote_asset')
+
+
+@admin.register(Strategy)
+class StrategyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Strategy._meta.get_fields()]
+    list_filter = ('side', )
+    search_fields = ('symbol', )
+    ordering = ('symbol', 'start_dt')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.get_fields()]
+    list_filter = ('strategy', 'is_valid', 'status')
+    search_fields = ('strategy', )
+    ordering = ('time', 'strategy')
+
