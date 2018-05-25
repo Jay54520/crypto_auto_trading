4a5cfbb373f85d3c24389e5467bae682741c228d
揭思敏 <jsm0834@175game.com>
2018-05-25 20:57:55 +0800

    #5 保存 strategy 后，禁用相同 symbol 的所有订单

--- strategy/signals.py
+++ strategy/signals.py
@@ -7,9 +7,8 @@ from strategy.utils import generate_orders
 def create_orders(sender, instance, created, **kwargs):
     """根据策略创建订单"""
     strategy = instance
-    if not created:
-        # 修改策略后删除旧策略对应的订单，然后在下面重新生成
-        Order.objects.filter(strategy=strategy).delete()
+    # 禁用相同 symbol 的所有订单
+    Order.objects.filter(strategy__symbol=strategy.symbol).update(is_valid=False)
 
     orders = generate_orders(strategy.symbol, strategy.side, strategy.quantity, strategy.start_dt,
                              strategy.end_dt, strategy)
