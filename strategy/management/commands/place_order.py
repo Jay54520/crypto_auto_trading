70a721fa3f9c85680d29d25f1d6690e983cb9575
揭思敏 <jsm0834@175game.com>
2018-05-27 20:25:05 +0800

    #9 Fix: place_order management 命令错误

--- strategy/management/commands/place_order.py
+++ strategy/management/commands/place_order.py
@@ -4,7 +4,7 @@ import datetime
 from django.core.management.base import BaseCommand
 
 from strategy.models import Order
-from strategy.utils import update_symbol_info, place_test_order
+from strategy.utils import place_order
 
 
 class Command(BaseCommand):
@@ -18,5 +18,5 @@ class Command(BaseCommand):
             time__lt=next_minute
         )
         for order in orders:
-            place_test_order(order)
+            place_order(order)
         self.stdout.write('下单 {} 笔'.format(orders.count()))
