6d021fb4bb538c7002e27487006b19b7e4008b26
揭思敏 <jsm0834@175game.com>
2018-05-28 08:08:04 +0800

    创建订单时删除相同 symbol 并且没有下过单的所有订单
    因为这些订单价值不大

--- strategy/signals.py
+++ strategy/signals.py
@@ -7,6 +7,8 @@ from strategy.utils import generate_orders
 def create_orders(sender, instance, created, **kwargs):
     """根据策略创建订单"""
     strategy = instance
+    # 删除相同 symbol 并且没有下过单的所有订单
+    Order.objects.filter(strategy__symbol=strategy.symbol, order_id__isnull=True).delete()
     # 禁用相同 symbol 的所有订单
     Order.objects.filter(strategy__symbol=strategy.symbol).update(is_valid=False)
 
